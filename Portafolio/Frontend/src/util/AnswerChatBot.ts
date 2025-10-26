e77b8a72a2258296493087f19988c088c2058a3f
oscar-principi <principioscar89gmail.com>
2025-10-26 10:18:15 -0300

    Cambio mensajes terminal

--- Portafolio/Frontend/src/util/AnswerChatBot.ts
+++ Portafolio/Frontend/src/util/AnswerChatBot.ts
@@ -5,46 +5,46 @@ type BotResponse = {
 
 export const botResponses: BotResponse[] = [
   {
-    keywords: ["profesional - ver mi perfil profesional"],
+    keywords: ["profesional"],
     answer:
       "Soy desarrollador de software en formación, me gusta trabajar en soluciones fullstack. Tengo experiencia académica y personal en frontend, backend y base de datos.",
   },
   {
-    keywords: ["personal - ver mi perfil personal"],
+    keywords: ["personal"],
     answer:
       "Me apasiona programar, siempre con mates al lado, me encanta el rock y pasar tiempo con mis compañeras patudas. Siempre estoy aprendiendo algo nuevo para seguir formándome.",
   },
   {
-    keywords: ["experiencia - ver mi experiencia"],
+    keywords: ["experiencia"],
     answer:
       "Tengo experiencia académica y personal en desarrollo de aplicaciones con Blazor en frontend, .NET y Java en backend, y bases de datos SQL. Sigo aprendiendo para mejorar como desarrollador fullstack y construir soluciones completas que generen un impacto positivo en la sociedad.",
   },
   {
-    keywords: ["formacion - ver mi formacion"],
+    keywords: ["formacion"],
     answer:
       "Estoy terminando la Tecnicatura en Programación en la UTN. Aprendí sobre programación fullstack en aplicaciones web y de escritorio.",
   },
   {
-    keywords: ["tecnologias - ver las tecnologias que uso"],
+    keywords: ["tecnologias"],
     answer:
       "Tengo conocimientos en .NET, Java, Blazor, HTML, CSS, Bootstrap, Tailwind, uso herramientas como Visual Studio, Eclipse, SQL Server, MySQL, Git, Docker. Me gusta aprender nuevas tecnologías para mejorar mis habilidades para resolver problemas reales.",
   },
   {
-    keywords: ["meta - ver mi meta"],
+    keywords: ["meta"],
     answer:
       "Quiero seguir creciendo y desarrollárme como fullstack profesionalmente.",
   },
   {
-    keywords: ["redes - ver mis redes"],
+    keywords: ["redes"],
     answer:
       "Te dejo mis redes por si querés ver lo que hago y/o contactarme:\n🔗 LinkedIn: linkedin.com/in/oscar-principi/ \n💻 GitHub: github.com/oscar-principi",
   },
   {
-    keywords: ["clear - limpiar terminal"],
+    keywords: ["clear"],
     answer:""
   },
     {
-    keywords: ["exit - salir terminal"],
+    keywords: ["exit"],
     answer:""
   },
 ];
